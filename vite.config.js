890d89369e0c29f04f0d417b1a22c8490dc3b11a
fahadi04 <fahadmasrur786@gmail.com>
2025-01-02 19:09:20 +0530

    change the vite cnfig

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      src: "/src/api",
-    },
-  },
-  build: {
-    outDir: "../dist", // Specify output directory for production build
-  },
 });
